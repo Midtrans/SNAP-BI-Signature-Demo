f7753ef62911d224e7bc20892de36565a378054c
Rizda Dwi Prasetya <rizdaprasetya@users.noreply.github.com>
2024-03-06 16:47:58 +0700

    add sample code

---
+++ sample-code/symmetricSignature/xSignatureGenerationJava.java
@@ -0,0 +1,75 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
+import java.util.Base64;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+// import package this package https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    /*-- INPUT --*/
+    String clientSecret = "myclientsecret";
+    String httpMethod = "POST".toUpperCase();
+    String endpointUrl = "/v1.0/qr/qr-mpm-generate";
+    String accessToken = "myaccesstoken";
+    // ensure requestBody is a valid JSON string format, dont worry about whitespaces, it will be minified later
+    String requestBody = "{\"partnerReferenceNo\":\"2020102900000000000001\",\"amount\":{\"value\":\"12345678.00\",\"currency\":\"IDR\"},\"feeAmount\":{\"value\":\"12345678.00\",\"currency\":\"IDR\"},\"merchantId\":\"00007100010926\",\"terminalId\":\"213141251124\",\"validityPeriod\":\"2009-07-03T12:08:56-07:00\"}";
+    String timestampString = new java.util.Date().toInstant().toString();
+    // sample hardcoded timestamp value
+    // timestampString = "2024-03-05T10:37:16.196Z";
+
+    /*-- PERFORM: Lowercase(HexEncode(SHA-256(minify(RequestBody)))) --*/
+    String minifiedRequestBody = minifyRequestBody(requestBody);
+    String lowerHexSha256MinifiedRequestBody = sha256Hex(minifiedRequestBody).toLowerCase();
+
+    String combinedStringToSign = 
+      httpMethod + ":" + 
+      endpointUrl + ":" + 
+      accessToken + ":" + 
+      lowerHexSha256MinifiedRequestBody + ":" + 
+      timestampString;
+
+    /*-- SIGNATURE GENERATION --*/
+    String xSignatureString = hmacSha512(clientSecret, combinedStringToSign);
+
+    /*-- OUTPUT --*/
+    System.out.println("xSignatureString:");
+    System.out.println(xSignatureString);
+  }
+
+  private static String minifyRequestBody(String requestBody) throws JsonProcessingException {
+    // simple way: map JSON string as JSON Object then convert back to string, it will remove unnecessary whitespaces
+    return (new ObjectMapper())
+        .readTree(requestBody)
+        .toString();
+  }
+
+  private static String bytesToHex(byte[] bytes) {
+    StringBuilder hexString = new StringBuilder();
+    for (byte b : bytes) {
+      String hex = Integer.toHexString(0xFF & b);
+      if (hex.length() == 1) {
+        hexString.append('0');
+      }
+      hexString.append(hex);
+    }
+    return hexString.toString();
+  }
+
+  private static String sha256Hex(String input) throws NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    byte[] hash = digest.digest(input.getBytes());
+    return bytesToHex(hash);
+  }
+
+  private static String hmacSha512(String key, String input) throws NoSuchAlgorithmException, InvalidKeyException {
+    SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "HmacSHA512");
+    Mac mac = Mac.getInstance("HmacSHA512");
+    mac.init(secretKey);
+    byte[] hash = mac.doFinal(input.getBytes());
+    return Base64.getEncoder().encodeToString(hash);
+  }
+}
